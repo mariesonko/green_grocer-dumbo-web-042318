6070f012ae30b562722c94f336c51155651117a1
Marie Sonko <mariesonko62@gmail.com>
2018-04-23 03:22:16 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,88 +1,58 @@
-def consolidate_cart(cart)
-  consolidated_hash = {}
+def consolidate_cart(cart:[])
+  consolidated_cart = {}
   cart.each do |item|
-    item.each do |item_name, item_hash|
-        if consolidated_hash.key?(item_name) == false
-          consolidated_hash[item_name] = item_hash
-          consolidated_hash[item_name][:count] = 1
-        else
-          consolidated_hash[item_name][:count] += 1
-        end
-    end
-  end
-  consolidated_hash
+    item_name = item.keys[0]
+
+       consolidated_cart[item_name] = item.values[0]
+
+     if consolidated_cart[item_name][:count]
+      consolidated_cart[item_name][:count] += 1
+    else
+      consolidated_cart[item_name][:count] = 1
+     end
+   end
+
+  consolidated_cart
 end
 
-def apply_coupons(cart, coupon_array)
+def apply_coupons(cart:[], coupons:[])
+  coupons.each do |coupon|
+    coupon_name = coupon[:item]
+    coupon_item_num = coupon[:num]
+    cart_item = cart[coupon_name]
 
-  if cart.size == 0
-    return cart
-  elsif coupon_array.size == 0
-    return cart
-  else
-    consolidated_coupons_hash = {}
-    coupon_array.each do |coupon|
-      item_name = coupon[:item]
-      if consolidated_coupons_hash.key?(item_name) == false
-        coupon = coupon.merge({coupon_count: 1})
-        consolidated_coupons_hash[item_name] = coupon
-      else
-          consolidated_coupons_hash[item_name][:num] += coupon[:num]
-          consolidated_coupons_hash[item_name][:coupon_count] += 1
-      end
-    end
-    consolidated_coupons_hash.each do |consolidated_key, value|
-      if cart.key?(consolidated_key)
-        consolidated_coupon_number = consolidated_coupons_hash[consolidated_key][:num]
-        cart_item_count = cart[consolidated_key][:count]
-        coupon_item_price = consolidated_coupons_hash[consolidated_key][:cost]
-        coupon_count = consolidated_coupons_hash[consolidated_key][:coupon_count]
-        cart_item_count_after_coupon = cart_item_count - consolidated_coupon_number
-        cart_item_clearance = cart[consolidated_key][:clearance]
+    next if cart_item.nil? || cart_item[:count] < coupon_item_num
 
-        cart[consolidated_key][:count] = cart_item_count_after_coupon
-        cart["#{consolidated_key} W/COUPON"] = {price: coupon_item_price, clearance: cart_item_clearance, count: coupon_count}
+    cart_item[:count] -= coupon_item_num
 
+    coupon_in_cart = cart["#{coupon_name} W/COUPON"]
 
-      end
+    if coupon_in_cart
+      coupon_in_cart[:count] += 1
+    else
+      cart["#{coupon_name} W/COUPON"] = {
+        price: coupon[:cost],
+        clearance: cart_item[:clearance],
+        count: 1
+      }
     end
   end
+
   cart
 end
 
-def apply_clearance(cart)
-  cart.each do |item_name, item_hash|
-    if item_hash[:clearance] == true
-      
-      clearance_price = item_hash[:price] - (item_hash[:price] * 0.2)
-      item_hash[:price] = clearance_price
-    end
-  end
-  cart
-end
+def apply_clearance(cart:[])
 
-def checkout(cart, coupons)
+  cart = consolidate_cart(cart: cart)
+  cart = apply_coupons(cart: cart, coupons: coupons)
+  cart = apply_clearance(cart: cart)
   total = 0
-  cart = consolidate_cart(cart)
-  coupons_applied = apply_coupons(cart, coupons)
-
-  clearance_applied = apply_clearance(coupons_applied)
-
-  clearance_applied.each do |item, item_hash|
-    if item_hash[:count] < 0
-      item_hash[:count] = -(item_hash[:count])
-    end
-    if !item.include?('W/COUPON')
-      if clearance_applied[item][:count] < clearance_applied["#{item} W/COUPON"][:count]
-        clearance_applied["#{item} W/COUPON"][:count] = clearance_applied[item][:count]
-      end
-    end
-
-    total += (item_hash[:price] * item_hash[:count])
-    puts total
+  cart.each do |item_name, item_data|
+    total += (item_data[:price] * item_data[:count])
   end
-  if total >= 100
-    total = total - (total *0.10)
+
+  if total > 100
+    0.9 * total
   else
     total
   end
